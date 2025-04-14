b59f9960e3336dff57fd3756c97e38d82e9813df
MuziDxy <84348850+MuziDxy@users.noreply.github.com>
2025-04-14 16:21:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // 解析请求体
 app.use(express.json());
